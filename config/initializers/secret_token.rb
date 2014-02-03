6d538a959feba3bc6161916e3c2f3a98bf40386d
Ben Fitterman <ben.fitterman@gmail.com>
2014-02-03 18:42:54 -0500

    The Birth of the App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brazilenglish::Application.config.secret_key_base = '46a2693af873487be3a6485e831e7eacb9b7a5656cedfc9eb24644eaac5cd420615bdcf5bfb0abcfb065e639d46bcee516e4d14d588388cb00ab35002b3fd674'
