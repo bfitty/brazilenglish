7165c4d1cb6632fed5c90baef467c22a89e7fdc7
Ben Fitterman <ben.fitterman@gmail.com>
2014-02-03 19:31:28 -0500

    Added Home, about, lessons pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Brazilenglish::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "lessons" => "pages#lessons"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
